bb9d0806a48a97063616e90635d2e985406d4a38
Duncan Sparrell <duncan@sfractal.com>
2025-01-24 12:50:06 -0500

    more debugging

--- lib/cfsjksas_web/live/circle_live/ancestor_view.ex
+++ lib/cfsjksas_web/live/circle_live/ancestor_view.ex
@@ -8,31 +8,16 @@ defmodule CfsjksasWeb.CircleLive.AncestorView do
     svg_path = Cfsjksas.Circle.Get.path(:ancestors_svg)
     anc_ids_path = Cfsjksas.Circle.Get.path(:ancestors_ids)
     anc_rel_path = Cfsjksas.Circle.Get.path(:ancestors_relations)
-    IO.inspect(anc_rel_path, label: "anc_rel_path")
-    #dir = File.cwd!
-    #IO.inspect(dir, label: "dir")
-    exists = File.exists?(anc_rel_path)
-    IO.inspect(exists, label: "exists")
-    lsdir = File.ls("/app/lib/cfsjksas-0.1.0/priv/")
-    IO.inspect(lsdir, label: "lsdir")
-    lsdir2 = File.ls("/app/lib/cfsjksas-0.1.0/priv/static")
-    IO.inspect(lsdir2, label: "lsdir2")
-    lsdir3 = File.ls("/app/lib/cfsjksas-0.1.0/priv/static/gendata")
-    IO.inspect(lsdir3, label: "lsdir3")
-    IO.inspect("beyond lsdir3")
 
     # read in ancestor_rel map
     ## read in g.ex.txt file
     {:ok, raw_rel_text} = File.read(anc_rel_path)
 
-    IO.inspect("does it get beyond reading?")
     ## parse into elixir data
     {ancestors_relations, _bindings} = Code.eval_string(raw_rel_text)
-    IO.inspect("does it get beyond parsing?")
 
     gen_num = for gen <- 1..15, do: {gen, length(Map.keys(ancestors_relations[gen]))}
     total_anc = sum_anc(gen_num)
-    IO.inspect("does it get beyond counting ancestors?")
 
     # read in ancestor_ids map
     ## read in g.ex.txt file
@@ -44,6 +29,7 @@ defmodule CfsjksasWeb.CircleLive.AncestorView do
     circle_made = find_circle_made(svg_path)
 
     IO.inspect("does it get beyond circlemade?")
+    IO.inspect(circle_made, label: "circle_made")
 
     {:ok,
      socket
