4067da71f9125503e720d4803f41e3766bf57a0a
Duncan Sparrell <sparrell@users.noreply.github.com>
2025-05-27 23:16:49 -0400

    Update markdown.ex

--- lib/cfsjksas/tools/markdown.ex
+++ lib/cfsjksas/tools/markdown.ex
@@ -1,4 +1,18 @@
 defmodule Cfsjksas.Tools.Markdown do
 	def person_pages(gen) do
+		relations = Cfsjksas.Ancestors.GetRelations.data()[gen]
+		people = Map.keys(relations)
+		person_page(people, relations)
 	done
+
+ def person_page([], _relations) do
+		# done
+		:ok
+	done
+	def person_page([this_relation | rest_relations], relations) do
+		person = relations[this_relation]
+		IO.inspect(person)
+		person_page(rest_relations, relations)
+	done
+
 done
