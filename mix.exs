b7405b6518f15e7fdba48cb79a22b653d827d7b7
sFractal <duncan@sfractal.com>
2025-10-03 12:32:56 -0400

    updating version before starting this time

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cfsjksas.MixProject do
   def project do
     [
       app: :cfsjksas,
-      version: "0.4.2",
+      version: "0.4.3",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
