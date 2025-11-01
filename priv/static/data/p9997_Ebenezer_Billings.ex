6a3c18a017ba57bb824463885b84e92cd6805a7b
sFractal <duncan@sfractal.com>
2025-11-01 14:45:53 -0400

    check

---
+++ priv/static/data/p9997_Ebenezer_Billings.ex
@@ -0,0 +1,85 @@
+		##############  Ebenezer Billings
+		p9997: %{
+			also_known_as: nil,
+			baptism: nil,
+			birth_date: "xxx",
+			birth_note: nil,
+			birth_place: nil,
+            birth_source: nil,
+			birth_year: "xxx",
+			buried: nil,
+			census: [
+			],
+			christening: nil,
+			death_age: nil,
+			death_cause: nil,
+			death_date: "xxx",
+			death_note: nil,
+			death_place: xxx or nil,
+			death_source: nil,
+			death_year: "xxx",
+			description: nil,
+			education: [
+			],
+			emigration: [
+			],
+			event: [
+			],
+			family_of_origin: :xxx lookup or nil,
+			family_of_procreation: [
+				"xxx look up or empty list",
+			],
+			father: :xxx lookup or nil,
+			former_name: nil,
+			given_name: "first middle",
+			graduation: [
+			],
+			id: :xxx_look_up_or_makeup,
+			immigration: nil,
+			label: "genXxxx.Pxxx.Namexxx",
+			links: %{
+				geni: "https://www.geni.com/xxx",
+				myheritage: "https://www.myheritage.com/xxx",
+				werelate: "https://www.werelate.org/wiki/xxx",
+				wikitree: "https://www.wikitree.com/wiki/xxx",
+                other_links: %{
+					wikipedia: "https://en.wikipedia.org/xxx",
+				},
+			},
+			married_name: nil,
+			mh_famc: "xxx look up or nil",
+			mh_famc2: nil,
+			mh_fams: [
+				"xxx look up or empty list",
+			],
+			mh_id: "lookup or nil",
+			mh_name: "xxx First /Surname/",
+			mother: :xxx lookup or nil,
+			name_prefix: nil,
+			name_suffix: nil,
+			naturalized: nil,
+			nickname: nil,
+			notes: [
+			],
+			occupation: [
+			],
+			ordained: [
+			],
+			probate: nil,
+			relation_list: [
+				["P", xxx],
+			],
+			religion: nil,
+			residence: [
+			],
+			rin: "MH:xxxlookup",
+			sex: "MxxxF",
+			sources: [
+			],
+			surname: "xxx",
+			title: nil,
+			uid: "xxx",
+			upd: nil,
+			will: [
+			],
+		},
---
+++ priv/static/data/p9998_Jemina_Billings.ex
@@ -0,0 +1,80 @@
+		##############  Jemina Billings
+		p9998: %{
+			also_known_as: nil,
+			baptism: nil,
+			birth_date: "Apr 15 1695",
+			birth_note: nil,
+			birth_place: nil,
+            birth_source: nil,
+			birth_year: "1695",
+			buried: nil,
+			census: [
+			],
+			christening: nil,
+			death_age: nil,
+			death_cause: nil,
+			death_date: "Jan 23 1733",
+			death_note: nil,
+			death_place: nil,
+			death_source: nil,
+			death_year: "1733",
+			description: nil,
+			education: [
+			],
+			emigration: [
+			],
+			event: [
+			],
+			family_of_origin: nil,
+			family_of_procreation: [
+			],
+			father: :9997,
+			former_name: nil,
+			given_name: "Jemina",
+			graduation: [
+			],
+			id: :p9998,
+			immigration: nil,
+			label: "genXxxx.Pxxx.Namexxx",
+			links: %{
+				geni: "https://www.geni.com/xxx",
+				myheritage: "https://www.myheritage.com/xxx",
+				werelate: "https://www.werelate.org/wiki/xxx",
+				wikitree: "https://www.wikitree.com/wiki/xxx",
+			},
+			married_name: nil,
+			mh_famc: nil,
+			mh_famc2: nil,
+			mh_fams: [
+			],
+			mh_id: nil,
+			mh_name: "Jemina /Billings/",
+			mother: :9996,
+			name_prefix: nil,
+			name_suffix: nil,
+			naturalized: nil,
+			nickname: nil,
+			notes: [
+			],
+			occupation: [
+			],
+			ordained: [
+			],
+			probate: nil,
+			relation_list: [
+				["P", xxx],
+			],
+			religion: nil,
+			residence: [
+			],
+			rin: "MH:xxxlookup",
+			sex: "MxxxF",
+			sources: [
+			],
+			surname: "xxx",
+			title: nil,
+			uid: "xxx",
+			upd: nil,
+			will: [
+			],
+		},
