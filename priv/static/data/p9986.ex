7e329948a13a3fba836aef4fb7d6f77e83ded275
sFractal <duncan@sfractal.com>
2025-12-14 23:32:12 -0500

    fisher up to ann pierce

---
+++ priv/static/data/p9986.ex
@@ -0,0 +1,76 @@
+		##############  Frances Father Unknown-*
+		p9986: %{
+			also_known_as: nil,
+			baptism: nil,
+			birth_date: nil,
+			birth_note: nil,
+			birth_place: nil,
+			birth_source: nil,
+			birth_year: "?",
+			buried: nil,
+			census: [
+			],
+			christening: nil,
+			death_age: nil,
+			death_cause: nil,
+			death_date: nil,
+			death_note: nil,
+			death_place: nil,
+			death_source: nil,
+			death_year: "?",
+			description: nil,
+			education: [
+			],
+			emigration: [
+			],
+			event: [
+			],
+			family_of_origin: nil,
+			family_of_procreation: [
+			],
+			father: nil,
+			former_name: nil,
+			given_name: "Frances Father",
+			graduation: [
+			],
+			id: :p9986,
+			immigration: nil,
+			label: "gen3.MPP.Frances_Father_Unknown".
+			links: %{
+			},
+			married_name: nil,
+			mh_famc: nil,
+			mh_famc2: nil,
+			mh_fams: [
+			],
+			mh_id: nil,
+			mh_name: "Frances Father /Unknown/",
+			mother: nil,
+			name_prefix: nil,
+			name_suffix: nil,
+			naturalized: nil,
+			nickname: nil,
+			notes: [
+			],
+			occupation: [
+			],
+			ordained: [
+			],
+			probate: nil,
+			relation_list: [
+				["M", "P", "P"],
+			],
+			religion: nil,
+			residence: [
+			],
+			rin: nil,
+			sex: "M",
+			sources: [
+			],
+			surname: "Unknown-*",
+			title: nil,
+			uid: nil,
+			upd: nil,
+			will: [
+			],
+		},
